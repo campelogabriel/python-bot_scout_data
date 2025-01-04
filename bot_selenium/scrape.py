f69aa11080fc2be8c589e774efa74d1956f7ff34
GabrielCampelo1998 <gabrielcampelo98@outlook.com>
2025-01-04 13:50:08 -0300

    finish bot scrap

--- bot_selenium/scrape.py
+++ bot_selenium/scrape.py
@@ -54,52 +54,22 @@ competition_btns = driver.find_elements(By.CLASS_NAME,"jFxLbA")
 
 # DADOS DE SCOUT
 
-scout_partidas = {
-    "partidas_jogadas": 0,
-    "minutagem": 0,
-}
-scout_ataque = {
-    "gols":0,
-    "xG" : 0.0,
-    "chutes_por_jogo": 0.0,
-    "chutes_no_alvo": 0.0,
-    "grandes_chances_perdidas":0,
-}
-scout_passes = {
-    "assistencias":0,
-    "xA": 0.0,
-    "grandes_chances_criadas": 0,
-    "acerto_no_passe" : "0%",
-    "acerto_na_bola_longa": "0%"
-}
+scout_new = {}
 
-scout_defesas = {
-    "interpcoes_por_jogo" : 0.0,
-    "bolas_recuperdas_por_jogo": 0.0,
-    "desarmes_por_jogo" : 0.0,
-    "dribles_sofridos_por_jogo": 0.0,
-    "erros_que_levaram_ao_chute": 0,
-    "errors_que_levaram_ao_gol": 0
-}
-scout_outros = {
-    "sucesso_nos_dribles" : "0%",
-    "duelos_vencidos": "0%",
-    "duelos_no_chão_vencidos": "0%",
-    "duelos_aereos_vencidos": "0%",
-    "posse_perdida": 0.0
-}
-
-sleep(13)
+sleep(20)
 
 blocks = driver.find_elements(By.CSS_SELECTOR,"div[class='Box Flex VhXzF kGBmhP']")[1]
-
 blocks_scout = blocks.find_elements(By.CSS_SELECTOR,"div[class='Box jfMEge']")
-for i,bl in enumerate(blocks_scout):
 
+n = 0
+for i,bl in enumerate(blocks_scout):
     if i < 2:
         continue    
-    print("*********** ", i)
-    print(bl.text)
+    texto = bl.text.split("\n")[1:]
+    result = {texto[n]: texto[n + 1] for n in range(0, len(texto), 2)}
+    scout_new.update(result)
+
+print(scout_new)
 
 sleep(60)
 
