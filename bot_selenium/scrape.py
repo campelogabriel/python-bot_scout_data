6d415624100fe89150e01e04ab5030fe82bdf292
GabrielCampelo1998 <gabrielcampelo98@outlook.com>
2025-01-04 12:12:27 -0300

    feat: scout dict

--- bot_selenium/scrape.py
+++ bot_selenium/scrape.py
@@ -34,23 +34,72 @@ except StaleElementReferenceException:
 
 sleep(15)
 
-btn = driver.find_elements(By.CLASS_NAME,"jQruaf")
+btn = driver.find_elements(By.CSS_SELECTOR,"div[class='Dropdown gSFIyj']")
 
 for i,n in enumerate(btn):
    if i == 1:
        n.click()
 
 
-# COMPETITION = "Carioca"
+# AQUI ESCOLHE A COMPETICAO
 
-# competition_btns = driver.find_elements(By.CLASS_NAME,"jFxLbA")
+COMPETITION = "Carioca"
 
-# print(competition_btns)
+competition_btns = driver.find_elements(By.CLASS_NAME,"jFxLbA")
 
 # for btn in competition_btns:
 #     if COMPETITION in btn.text:
 #         btn.click()
 
+
+# DADOS DE SCOUT
+
+scout_partidas = {
+    "partidas_jogadas": 0,
+    "minutagem": 0,
+}
+scout_ataque = {
+    "gols":0,
+    "xG" : 0.0,
+    "chutes_por_jogo": 0.0,
+    "chutes_no_alvo": 0.0,
+    "grandes_chances_perdidas":0,
+}
+scout_passes = {
+    "assistencias":0,
+    "xA": 0.0,
+    "grandes_chances_criadas": 0,
+    "acerto_no_passe" : "0%",
+    "acerto_na_bola_longa": "0%"
+}
+
+scout_defesas = {
+    "interpcoes_por_jogo" : 0.0,
+    "bolas_recuperdas_por_jogo": 0.0,
+    "desarmes_por_jogo" : 0.0,
+    "dribles_sofridos_por_jogo": 0.0,
+    "erros_que_levaram_ao_chute": 0,
+    "errors_que_levaram_ao_gol": 0
+}
+scout_outros = {
+    "sucesso_nos_dribles" : "0%",
+    "duelos_vencidos": "0%",
+    "duelos_no_chão_vencidos": "0%",
+    "duelos_aereos_vencidos": "0%",
+    "posse_perdida": 0.0
+}
+
+sleep(13)
+
+blocks = driver.find_elements(By.CSS_SELECTOR,"div[class='Box Flex VhXzF kGBmhP']")[1]
+
+blocks_scout = blocks.find_elements(By.CSS_SELECTOR,"div[class='Box jfMEge']")
+for i,bl in enumerate(blocks_scout):
+
+    if i < 2:
+        continue    
+    print("*********** ", i)
+    print(bl.text)
 
 sleep(60)
 
