1489ce3f4e532f809c9441b5cfce9233f5961bfa
GabrielCampelo1998 <gabrielcampelo98@outlook.com>
2025-01-08 18:54:06 -0300

    fix: bug bot, add tag selector

--- bot_selenium/scrape.py
+++ bot_selenium/scrape.py
@@ -14,9 +14,9 @@ driver.maximize_window()
 input_element = driver.find_element(By.ID,"search-input")
 
 if input_element.is_displayed():
-    input_element.send_keys("paulinho paula")
+    input_element.send_keys("Murillo")
 
-
+# COMPETITION
 try:
     btn_element = driver.find_element(By.CLASS_NAME,"sc-44409fa7-0")
     if btn_element.is_displayed():
@@ -47,19 +47,21 @@ COMPETITION = "Carioca"
 
 competition_btns = driver.find_elements(By.CLASS_NAME,"jFxLbA")
 
-# for btn in competition_btns:
-#     if COMPETITION in btn.text:
-#         btn.click()
-
-
 # DADOS DE SCOUT
 
 scout_new = {}
 
-sleep(20)
+sleep(10)
 
-blocks = driver.find_elements(By.CSS_SELECTOR,"div[class='Box Flex VhXzF kGBmhP']")[1]
-blocks_scout = blocks.find_elements(By.CSS_SELECTOR,"div[class='Box jfMEge']")
+blocks = driver.find_elements(By.CSS_SELECTOR,"div[class='Box Flex VhXzF kGBmhP']")
+
+if len(blocks) < 2:
+    sleep(40)
+    blocks = driver.find_elements(By.CSS_SELECTOR,"div[class='Box Flex VhXzF kGBmhP']")
+
+blocks_scout = blocks[1].find_elements(By.CSS_SELECTOR,"div[class='Box jfMEge']")
+
+
 
 n = 0
 for i,bl in enumerate(blocks_scout):
@@ -67,11 +69,39 @@ for i,bl in enumerate(blocks_scout):
         continue    
     texto = bl.text.split("\n")[1:]
     result = {texto[n]: texto[n + 1] for n in range(0, len(texto), 2)}
+    print(result)
     scout_new.update(result)
+
+profile = {'profile': ""}
+
+
+try:
+    profile['profile'] = driver.find_element(
+        By.CLASS_NAME,"bFoDOL").get_attribute("src")
+except NoSuchElementException:
+    print("exception")
+    sleep(10)
+    profile['profile'] = driver.find_element(
+        By.CLASS_NAME,"bFoDOL").get_attribute("src")
+
+
+team_info = {}
+player_info = {}
+
+team_element = driver.find_element(By.CSS_SELECTOR,"div[class='Box Flex bsXxAs jLRkRA']")
+
+team_info['name_team'] = team_element.find_element(By.CSS_SELECTOR,"div[class='Text leMLNz']").text
+team_info['img_team'] = team_element.find_element(By.TAG_NAME,"img").get_attribute("src")
+
+player_element = driver.find_element(By.CSS_SELECTOR,"div[class='Box Flex ggRYVx flkZQO']")
+
+scout_new.update(team_info)
+scout_new.update(profile)
 
 print(scout_new)
 
-sleep(60)
+
+sleep(10)
 
 driver.quit()
 
