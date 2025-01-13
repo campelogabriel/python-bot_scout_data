4dfee1aa530e308c316a72a42eada991f4ec36f9
GabrielCampelo1998 <gabrielcampelo98@outlook.com>
2025-01-13 20:19:29 -0300

    finish pdf file generate

--- generate_pdf/render_pdf.py
+++ generate_pdf/render_pdf.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageDraw
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
-from reportlab.lib.colors import black,white,gray,gold,whitesmoke
+from reportlab.lib.colors import black,white,gray,gold,whitesmoke,lightgray
 from datetime import datetime
 from .get_positions import get_positions
 from .position_player import position_convert
@@ -57,7 +57,12 @@ def create_pdf(obj):
     pdf.rect(x=0,y= 0,height=y,width= x/4,stroke=0, fill=1)
     pdf.setFillColor(whitesmoke)
     pdf.rect(x=x/4,y=0,width= x - x/4 ,height=y,stroke=0,fill=1)
-    pdf.drawImage(obj['img_team'],120,y - 74,60,60, mask="auto")
+    
+    
+    try:
+        pdf.drawImage(obj['img_team'],119.5,y - 74,60,60, mask="auto")
+    except Exception:
+        ...
 
     round_image_with_border(obj['profile'],"imagem_arredondada.png",60,0) 
 
@@ -70,51 +75,61 @@ def create_pdf(obj):
 
     pdf.setFillColor(white)
     centralizar_texto_area(pdf,position_convert(obj['pos']),fonte="Helvetica",x_inicio=45,largura_area=60, y= y - 150,tamanho_fonte=10)
+    
 
 
-
-    pdf.setFont("Helvetica",9)
+    pdf.setFont("Helvetica-Bold",9)
     pdf.setFillColor(gray)
     pdf.drawString(x=10,y= y - 190,text="Data Nasc")
     pdf.setFillColor(white)
     pdf.setFont("Helvetica-Bold",9)
     pdf.drawString(x=9,y= y - 205,text=datetime.strptime(obj['nascimento'], "%d %b %Y").strftime("%d/%m/%Y"))
 
+    pdf.setFont("Helvetica-Bold",9)
     pdf.setFillColor(gray)
-    pdf.setFont("Helvetica",9)
     pdf.drawString(x=80,y= y - 190,text="Pé Dominante")
     pdf.setFillColor(white)
-    centralizar_texto_area(pdf,"Direito" if obj['pé'] == "Right" else "Esquerdo",60,95,y - 205,tamanho_fonte=9,fonte="Helvetica-Bold")
+    centralizar_texto_area(pdf,"Direito" if obj['pé'] == "Right" else "Esquerdo",62,95,y - 205,tamanho_fonte=9,fonte="Helvetica-Bold")
 
 
+    pdf.setFont("Helvetica-Bold",9)
     pdf.setFillColor(gray)
-    pdf.setFont("Helvetica",9)
     pdf.drawString(x=17,y= y - 230,text="Altura")
     pdf.setFillColor(white)
     pdf.setFont("Helvetica-Bold",9)
-    pdf.drawString(x=14,y= y - 245,text=f"{obj['altura']} cm")
+    pdf.drawString(x=15,y= y - 245,text=f"{obj['altura']} cm")
+    
 
+    pdf.drawImage(obj['bandeira'],90,y - 246,10,10,mask='auto')
+    pdf.setFont("Helvetica-Bold",9)
     pdf.setFillColor(gray)
-    pdf.setFont("Helvetica",9)
     pdf.drawString(x=80,y= y - 230,text="Nacionalidade")
     pdf.setFillColor(white)
     pdf.setFont("Helvetica-Bold",9)
-    pdf.drawString(x=98,y= y - 245,text=obj['nacionalidade'])
+    pdf.drawString(x=105,y= y - 245,text=obj['nacionalidade'])
 
     pdf.setStrokeColor(white)
     pdf.line(0,y - 260,x/4,y - 260)
 
-    centralizar_texto_area(pdf,"Última Partida",40,67,y - 280,tamanho_fonte=10,fonte="Helvetica-Bold")
-    centralizar_texto_area(pdf,obj['data'],40,67,y - 295,tamanho_fonte=8,fonte="Helvetica")
+    if 'placar_mandante' in obj:
+        centralizar_texto_area(pdf,"Última Partida",40,67,y - 280,tamanho_fonte=10,fonte="Helvetica-Bold")
+        centralizar_texto_area(pdf,obj['data'] if obj['data'] else "Em Andamento",39,67,y - 295,tamanho_fonte=8,fonte="Helvetica")
+    else:
+        centralizar_texto_area(pdf,"Próxima Partida",40,67,y - 280,tamanho_fonte=10,fonte="Helvetica-Bold")
 
 
 
     pdf.drawImage(obj['escudo_mandante'],18,y - 335,20,20, mask="auto")
     centralizar_texto_area(pdf,obj['nome_mandante'],8,40,y - 350,tamanho_fonte=8,fonte="Helvetica")
 
-    centralizar_texto_area(pdf,obj['placar_mandante'],36,50,y - 333,tamanho_fonte=20,fonte="Helvetica-Bold")
-    centralizar_texto_area(pdf,"-",44,55,y - 332,tamanho_fonte=14,fonte="Helvetica-Bold")
-    centralizar_texto_area(pdf,obj['placar_visitante'],52,60,y - 333,tamanho_fonte=20,fonte="Helvetica-Bold")
+    
+    if 'placar_mandante' in obj:
+        centralizar_texto_area(pdf,obj['placar_mandante'],37,50,y - 333,tamanho_fonte=20,fonte="Helvetica-Bold")
+        centralizar_texto_area(pdf,"-",44,55,y - 332,tamanho_fonte=14,fonte="Helvetica-Bold")
+        centralizar_texto_area(pdf,obj['placar_visitante'],52,60,y - 333,tamanho_fonte=20,fonte="Helvetica-Bold")
+    else:
+        centralizar_texto_area(pdf,obj['data'].split(" ")[0],40,67,y - 295,tamanho_fonte=9,fonte="Helvetica")
+        centralizar_texto_area(pdf,obj['data'].split(" ")[1],44,55,y - 330,tamanho_fonte=12,fonte="Helvetica-Bold")
 
 
     pdf.drawImage(obj['escudo_visitante'],x / 4 - 45,y - 335,20,20, mask="auto")
@@ -125,28 +140,32 @@ def create_pdf(obj):
     pdf.setStrokeColor(white)
     pdf.line(0,y - 380,x/4,y - 380)
     pdf.setFont("Helvetica-Bold",10)
-    pdf.drawString(53,y-400,"Posição")
-    pdf.drawImage("images/soccer-field.png",1.5,y - 560,140,140, mask="auto")
+    pdf.drawString(53,y-405,"Posição")
+    pdf.drawImage("images/field.png",1.5,y - 560,140,140, mask="auto")
 
     # POSICOES
     pdf.setFont("Helvetica-Bold",8)
     for pos in obj['positions']:
         get_positions(pos,pdf,y)
    
-
+    pdf.drawImage("images/x_logo.png",25,y - 660,15,15,mask='auto')
+    pdf.drawImage("images/logo.png",55,y - 640,35,35,mask='auto')
+    pdf.setFont("Helvetica-Bold",8)
+    pdf.setFillColor(white)
+    pdf.drawString(44,y - 655,"@datascoutme")
     #PARTE DO SCOUT
 
     #TITULO
 
-    pdf.drawImage(obj['img_liga'],x-40,y-45,30,30,mask='auto')
     pdf.setFillColor(black)
-    centralizar_texto_area(pdf,f"{obj['liga']} {obj['temp']}",220,300,y - 40,"Courier-Bold",22)
+    centralizar_texto_area(pdf,f"{obj['liga']} {obj['temp']}",240,300,y - 40,"Courier-Bold",22)
 
     # PARTIDAS
-    
+    pdf.setFillColor(lightgray)
+    pdf.roundRect(183,y-243,169.5,140,8,stroke=0,fill=1)
     pdf.setFillColor(white)
-    pdf.roundRect(180,y-240,170,140,5,stroke=0,fill=1)
-
+    pdf.roundRect(180,y-240,170,140,5,stroke=1,fill=1)
+  
 
     pdf.drawImage("images/partidas.png",335,y-115,30,30,mask='auto')
 
@@ -154,7 +173,7 @@ def create_pdf(obj):
     pdf.setFont("Courier-Bold",12)
     pdf.drawString(190, y - 120,"Partidas")
     pdf.setFont("Courier-Bold",24)
-    pdf.drawString(190, y - 150,obj['Total_played'])
+    pdf.drawString(190, y - 150,f"{obj['Total_played']} ")
 
     # DADOS PARTIDAS
     pdf.setFillColor(gray)
@@ -179,9 +198,10 @@ def create_pdf(obj):
     pdf.drawString(260, y - 201,obj['Total_minutes_played'])
 
     # Gols
-
+    pdf.setFillColor(lightgray)
+    pdf.roundRect(408,y-243,169.5,140,8,stroke=0,fill=1)
     pdf.setFillColor(white)
-    pdf.roundRect(405,y-240,170,140,5,stroke=0,fill=1)
+    pdf.roundRect(405,y-240,170,140,5,stroke=1,fill=1)
 
 
     pdf.drawImage("images/gols.png",560,y-113,25,25,mask='auto')
@@ -231,9 +251,10 @@ def create_pdf(obj):
     pdf.drawString(485, y - 227,obj['Headed_goals'])
 
     # Passe
-
+    pdf.setFillColor(lightgray)
+    pdf.roundRect(183,y-413,169.5,140,8,stroke=0,fill=1)
     pdf.setFillColor(white)
-    pdf.roundRect(180,y-410,170,140,5,stroke=0,fill=1)
+    pdf.roundRect(180,y-410,170,140,5,stroke=1,fill=1)
 
 
     pdf.drawImage("images/assistencias.png",337,y-283,30,30,mask='auto')
@@ -282,9 +303,10 @@ def create_pdf(obj):
     pdf.drawString(293, y - 397,obj['Acc._crosses'])
 
     # defesa
-
+    pdf.setFillColor(lightgray)
+    pdf.roundRect(408,y-413,169.5,140,8,stroke=0,fill=1)
     pdf.setFillColor(white)
-    pdf.roundRect(405,y - 410,170,140,5,stroke=0,fill=1)
+    pdf.roundRect(405,y - 410,170,140,5,stroke=1,fill=1)
 
 
     pdf.drawImage("images/desarmes.png",560,y-283,30,30,mask='auto')
@@ -329,15 +351,16 @@ def create_pdf(obj):
 
     pdf.setFillColor(gray)
     pdf.setFont("Helvetica",9)
-    pdf.drawString(415, y - 397,"Duelos Aereos Vencidos:")
+    pdf.drawString(415, y - 397,"Duelos Aéreos Vencidos:")
     pdf.setFillColor(black)
     pdf.setFont("Helvetica-Bold",9)
     pdf.drawString(518, y - 397,obj['Aerial_duels_won'])
 
     # Outros
-
+    pdf.setFillColor(lightgray)
+    pdf.roundRect(183,y-583,169.5,140,8,stroke=0,fill=1)
     pdf.setFillColor(white)
-    pdf.roundRect(180,y-580,170,140,5,stroke=0,fill=1)
+    pdf.roundRect(180,y-580,170,140,5,stroke=1,fill=1)
 
   
     pdf.drawImage("images/outros.png",337,y-450,20,20,mask='auto')
@@ -349,38 +372,38 @@ def create_pdf(obj):
 
     pdf.setFillColor(gray)
     pdf.setFont("Helvetica",9)
-    pdf.drawString(190, y - 485,"Dribles Completos:")
+    pdf.drawString(190, y - 485,"Dribles Completos por jogo:")
     pdf.setFillColor(black)
     pdf.setFont("Helvetica-Bold",9)
-    pdf.drawString(268, y - 485,obj['Succ._dribbles'])
+    pdf.drawString(304, y - 485,obj['Succ._dribbles'])
 
     pdf.setFillColor(gray)
     pdf.setFont("Helvetica",9)
     pdf.drawString(190, y - 498,"Perda da bola por jogo:")
     pdf.setFillColor(black)
     pdf.setFont("Helvetica-Bold",9)
-    pdf.drawString(288, y - 498,obj['Possession_lost'])
+    pdf.drawString(285, y - 498,obj['Possession_lost'])
 
     pdf.setFillColor(gray)
     pdf.setFont("Helvetica",9)
-    pdf.drawString(190, y - 511,"Faltas Sofridas:")
+    pdf.drawString(190, y - 511,"Faltas Sofridas por jogo:")
     pdf.setFillColor(black)
     pdf.setFont("Helvetica-Bold",9)
-    pdf.drawString(255, y - 511,obj['Was_fouled'])
+    pdf.drawString(291, y - 511,obj['Was_fouled'])
 
     pdf.setFillColor(gray)
     pdf.setFont("Helvetica",9)
-    pdf.drawString(190, y - 524,"Faltas Cometidas:")
+    pdf.drawString(190, y - 524,"Faltas Cometidas por jogo:")
     pdf.setFillColor(black)
     pdf.setFont("Helvetica-Bold",9)
-    pdf.drawString(265, y - 524,obj['Fouls'])
+    pdf.drawString(300, y - 524,obj['Fouls'])
 
     pdf.setFillColor(gray)
     pdf.setFont("Helvetica",9)
-    pdf.drawString(190, y - 537,"Impedimentos:")
+    pdf.drawString(190, y - 537,"Impedimentos por jogo:")
     pdf.setFillColor(black)
     pdf.setFont("Helvetica-Bold",9)
-    pdf.drawString(251, y - 537,obj['Offsides'])
+    pdf.drawString(286, y - 537,obj['Offsides'])
 
 
     pdf.setFillColor(gray)
@@ -388,7 +411,7 @@ def create_pdf(obj):
     pdf.drawString(190, y - 549,"Erros que levaram ao chute:")
     pdf.setFillColor(black)
     pdf.setFont("Helvetica-Bold",9)
-    pdf.drawString(306, y - 549,obj['Error_led_to_shot'])
+    pdf.drawString(305, y - 549,obj['Error_led_to_shot'])
 
     pdf.setFillColor(gray)
     pdf.setFont("Helvetica",9)
@@ -399,9 +422,10 @@ def create_pdf(obj):
 
 
     # CARTOES
-
+    pdf.setFillColor(lightgray)
+    pdf.roundRect(408,y-583,169.5,140,8,stroke=0,fill=1)
     pdf.setFillColor(white)
-    pdf.roundRect(405,y-580,170,140,5,stroke=0,fill=1)
+    pdf.roundRect(405,y-580,170,140,5,stroke=1,fill=1)
 
 
     pdf.drawImage("images/cartoes.png",561,y-457,30,30,mask='auto')
@@ -432,4 +456,7 @@ def create_pdf(obj):
     pdf.setFont("Helvetica-Bold",9)
     pdf.drawString(498, y - 511,obj['Red_cards'])
 
+    pdf.setFillColor(gray)
+    pdf.setFont("Helvetica-Bold",9)
+    pdf.drawString(x - 150,10,"*Dados obtidos no Sofascore")
     pdf.save()
